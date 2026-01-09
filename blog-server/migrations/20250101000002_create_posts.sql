7b632fa4f4f6db077c989c03d9b33b6cd9bbc8e3
eugen <b-eugen@yandex.ru>
2026-01-10 01:08:14 +0300

    1. add migration code

--- blog-server/migrations/20250101000002_create_posts.sql
+++ blog-server/migrations/20250101000002_create_posts.sql
@@ -4,11 +4,11 @@ CREATE TABLE IF NOT EXISTS posts (
     content TEXT NOT NULL,
     author_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     CONSTRAINT fk_posts_author
     FOREIGN KEY (author_id)
     REFERENCES users(id)
     ON DELETE CASCADE
 );
-CREATE INDEX IF NOT EXISTS idx_posts_created_at ON users(created_at);
-CREATE INDEX IF NOT EXISTS idx_posts_updated_at ON users(updated_at);
+CREATE INDEX IF NOT EXISTS idx_posts_created_at ON posts(created_at);
+CREATE INDEX IF NOT EXISTS idx_posts_updated_at ON posts(updated_at);
