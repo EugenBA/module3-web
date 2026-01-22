dc1fe6ee88971fc3b606e046f338f9629737c982
eugen <b-eugen@yandex.ru>
2026-01-22 18:00:18 +0300

    1. Add libs client 2. Refactor code

--- blog-client/build.rs
+++ blog-client/build.rs
@@ -1,5 +1,11 @@
-fn main() {
-    // Этот код выполняется перед сборкой
-
-    println!("cargo:rerun-if-changed=proto/blog.proto")
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=proto/blog.proto");
+    tonic_prost_build::configure()
+        .build_server(false)
+        .build_client(true)
+        .compile_protos(
+            &["proto/blog.proto"],
+            &["proto"],
+        )?;
+    Ok(())
 }
